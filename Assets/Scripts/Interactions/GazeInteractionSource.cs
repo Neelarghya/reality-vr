534acc324a06ac3ad84fa5a6c8ac9eef34c0b956
Neelarghya Mandal <nmandal@lenovo.com>
2020-08-01 20:25:51 +0530

    [Neel] Adding Reticle

--- Assets/Scripts/Interactions/GazeInteractionSource.cs
+++ Assets/Scripts/Interactions/GazeInteractionSource.cs
@@ -1,5 +1,6 @@
 ﻿using Google.XR.Cardboard;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace Interactions
@@ -7,7 +8,11 @@ namespace Interactions
     public class GazeInteractionSource : MonoBehaviour
     {
         [SerializeField] private float intractableDistance = 10;
-        
+
+        [SerializeField] private UnityEvent onFocusIntractable;
+        [SerializeField] private UnityEvent onLoseFocus;
+        [SerializeField] private UnityEvent onClick;
+
         private GameObject _gazedObject;
         private PointerEventData _eventData;
 
@@ -27,23 +32,40 @@ namespace Interactions
             {
                 if (_gazedObject != hit.transform.gameObject)
                 {
-                    if (_gazedObject) 
+                    if (_gazedObject)
+                    {
                         _gazedObject.GetComponent<IPointerExitHandler>()?.OnPointerExit(_eventData);
-                    
+
+                        if (IsGazedObjectIntractable())
+                            onLoseFocus?.Invoke();
+                    }
+
                     _gazedObject = hit.transform.gameObject;
                     _gazedObject.GetComponent<IPointerEnterHandler>()?.OnPointerEnter(_eventData);
+
+                    if (IsGazedObjectIntractable())
+                        onFocusIntractable?.Invoke();
                 }
             }
             else if (_gazedObject)
             {
                 _gazedObject.GetComponent<IPointerExitHandler>()?.OnPointerExit(_eventData);
                 _gazedObject = null;
+                onLoseFocus?.Invoke();
             }
-            
+
             if (_gazedObject != null && Api.IsTriggerPressed)
             {
                 _gazedObject.GetComponent<IPointerClickHandler>()?.OnPointerClick(_eventData);
+
+                if (IsGazedObjectIntractable())
+                    onClick?.Invoke();
             }
+        }
+
+        private bool IsGazedObjectIntractable()
+        {
+            return _gazedObject.GetComponent<IEventSystemHandler>() != null;
         }
     }
 }
---
+++ Assets/Scripts/Interactions/ReticleController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Interactions
+{
+    [RequireComponent(typeof(Animator))]
+    public class ReticleController : MonoBehaviour
+    {
+        private Animator _animator;
+        private static readonly int IsOpen = Animator.StringToHash("isOpen");
+        private static readonly int Click = Animator.StringToHash("click");
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void ActivateReticle()
+        {
+            _animator.SetBool(IsOpen, true);
+        }
+
+        public void DeactivateReticle()
+        {
+            _animator.SetBool(IsOpen, false);
+        }
+
+        public void ClickReticle()
+        {
+            _animator.SetTrigger(Click);
+        }
+    }
+}
